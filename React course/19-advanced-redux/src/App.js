52e1a76f85eb9c5ce220868f3bacd20c3ae4b343
yoonsikga <yoonsikga@gmail.com>
2024-10-30 18:39:50 +0100

    Moved the dispatch logic from useEffect in App.js to a action creators in cart-slice

--- React course/19-advanced-redux/src/App.js
+++ React course/19-advanced-redux/src/App.js
@@ -4,8 +4,8 @@ import { Fragment, useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import {sendCartData} from './store/cart-slice';
 
 let isInitial = true;
 
@@ -16,45 +16,13 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data ",
-        })
-      );
-      const response = await fetch(
-        "https://react-course-bcd9f-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "error error error retard!",
-        })
-      );
-    });
+
+    dispatch(sendCartData(cart));
+    
   }, [cart, dispatch]);
 
   return (
--- React course/19-advanced-redux/src/store/cart-slice.js
+++ React course/19-advanced-redux/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -37,6 +38,51 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data ",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-course-bcd9f-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("sending cart data failed, retard");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "error error error retard!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
