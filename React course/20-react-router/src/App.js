56fea44acd28675220d17c91435d7bfeeb57d398
yoonsikga <yoonsikga@gmail.com>
2024-11-11 13:52:49 +0100

    Defining and using dynamic routes

--- React course/20-react-router/src/App.js
+++ React course/20-react-router/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // https://example.com/path
 
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage/>}
     ],
   },
 ]);
