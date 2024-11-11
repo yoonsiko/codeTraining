9058db4f3253b11b16ba0802e7d84318a4660658
yoonsikga <yoonsikga@gmail.com>
2024-11-11 10:52:18 +0100

    Added a second page with routing

--- React course/20-react-router/src/App.js
+++ React course/20-react-router/src/App.js
@@ -1,9 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
 // https://example.com/path
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage/>},
+]);
 
 function App() {
   return <RouterProvider router={router} />;
