15e63242b73e73afc03bde34e24db918023e48db
yoonsikga <yoonsikga@gmail.com>
2024-11-11 11:39:36 +0100

    Added styling and implemented the root and children property of MainNavigation component

--- React course/20-react-router/src/App.js
+++ React course/20-react-router/src/App.js
@@ -1,12 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 // https://example.com/path
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage/>},
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 function App() {
