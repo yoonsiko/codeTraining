7fdfd50fb60833208cc8c86503626f8b2282c34a
yoonsikga <yoonsikga@gmail.com>
2024-11-11 10:45:49 +0100

    Installed react router dom and created a dummy page for react routing testing purposes

--- React course/20-react-router/src/App.js
+++ React course/20-react-router/src/App.js
@@ -1,3 +1,13 @@
+import {createBrowserRouter} from 'react-router-dom'
+import HomePage from './pages/Home';
+
+// https://example.com/path
+
+createBrowserRouter([
+  {path:'/', element: <HomePage/>},
+  {}
+])
+
 function App() {
   return <div></div>;
 }
