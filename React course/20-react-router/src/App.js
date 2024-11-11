6e584d150bb6ca1881b4996a19cadac0a6234da8
yoonsikga <yoonsikga@gmail.com>
2024-11-11 11:00:42 +0100

    Reverting the alternative way for react routing to use the original method

--- React course/20-react-router/src/App.js
+++ React course/20-react-router/src/App.js
@@ -1,28 +1,13 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route
-} from "react-router-dom";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage/>} />
-    <Route path="/products" element={<ProductsPage/>} />
-  </Route>
-)
-
 // https://example.com/path
 
-/* const router = createBrowserRouter([
+const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
-]); */
-
-const router = createBrowserRouter(routeDefinitions)
+  { path: "/products", element: <ProductsPage/>},
+]);
 
 function App() {
   return <RouterProvider router={router} />;
