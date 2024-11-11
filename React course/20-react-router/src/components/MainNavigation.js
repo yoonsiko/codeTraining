faec2f9c61410b20e4cd30b5fd36f50a32d79695
yoonsikga <yoonsikga@gmail.com>
2024-11-11 13:37:45 +0100

    Implemented NavLink

--- React course/20-react-router/src/components/MainNavigation.js
+++ React course/20-react-router/src/components/MainNavigation.js
@@ -1,20 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css'
+import classes from "./MainNavigation.module.css";
 
-function MainNavigation(){
-  return <header className={classes.header}>
-    <nav>
-      <ul className={classes.list}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              /* style={({ isActive }) => (isActive ? "center" : "left")} */
+            >
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default MainNavigation
+export default MainNavigation;
