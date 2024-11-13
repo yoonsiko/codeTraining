41c42238b4840ffa9d0ab9cd294a0282cca0169a
yoonsik <yoonsikga@gmail.com>
2024-11-13 21:36:23 +0100

    Temperature converter app

---
+++ python-course/Exercises/tempConversion.py
@@ -0,0 +1,13 @@
+# Temperature conversion app
+
+unit = input("C or F? ")
+temp = float(input("Enter the temperature: "))
+
+if unit == "C":
+    temp = round((9 * temp) / 5 + 23, 1)
+    print(f"The temperature in Fahrenheit is: {temp}F")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in Celsius is: {temp}C")
+else:
+    print(f"{unit} is invalid unit")
