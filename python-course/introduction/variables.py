31f0cb390380d36f0ea47fcb047b93cf2bc0c4b6
yoonsik <yoonsikga@gmail.com>
2024-11-13 16:48:24 +0100

    node_modules be gone

---
+++ python-course/introduction/variables.py
@@ -0,0 +1,21 @@
+# Variable = A container for a value (string, integer, float, bool)
+#            A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Bro"
+
+# Integers
+quantity = 33
+# print(f"You are buying {quantity} items")
+
+# Float
+price = 10.99 
+# print(f"The price is {price}")
+
+# Boolean
+isStudent = False
+
+if isStudent: 
+    print("You are a student")
+else:
+    print("You are not a student")
---
+++ python-course/typecasting.py
@@ -0,0 +1,14 @@
+# Typecasting = the process of converting a variable from one data type to another str(), int(), float(), bool()
+
+name = "bro code"
+age = 25
+gpa = 3.2
+isStudent = True
+
+#print(type(age))
+
+#gpa = int(gpa)
+#print(gpa)
+
+age = float(age)
+print(age)
