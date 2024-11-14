7a09ae9c3ff23cffa1d2754ff5a65f21944d06b9
yoonsik <yoonsikga@gmail.com>
2024-11-14 16:16:23 +0100

    Conditional introduction

---
+++ python-course/introduction/conditional.py
@@ -0,0 +1,6 @@
+# conditional expression = A one line shortcut for the if else statement
+
+num = 5
+print("Positive" if num > 0 else "Negative")
+result = "EVEN" if num % 2 == 0 else "ODD"
+print(result)
