ec14357f1d846c8cbd49fdb3c5cef4e14059fc3e
yoonsik <yoonsikga@gmail.com>
2024-11-20 16:21:12 +0100

    nested loop

---
+++ python-course/introduction/nestedLoop.py
@@ -0,0 +1,7 @@
+# nested loop = A loop within another loop (outer, inner)
+#               outer loop:
+#                   inner loop:
+
+for i in range(3):
+    for j in range(1, 10):
+        print(f"{i}-{j}")
