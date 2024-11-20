8daf7d5f2ed51bb0ac5cc0c0458e7785566e1a3c
yoonsik <yoonsikga@gmail.com>
2024-11-20 16:13:45 +0100

    for loop

---
+++ python-course/introduction/forLoop.py
@@ -0,0 +1,20 @@
+# for loops = execute a block of code a fixed number of times.
+#             You can iterate over a range, string, sequence, etc.
+
+# for i in range(1, 5):
+#     print(i)
+
+# for i in range(1, 9, 3):
+#     print(i)
+
+# credit_card = "1234-5678"
+
+# for char in credit_card:
+#     print(char)
+
+
+for i in range(1, 21):
+    if i == 13:
+        break
+    else:
+        print(i)
