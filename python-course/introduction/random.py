9d621def2caab6f5a42a2e4126bbcb30499e85a1
yoonsik <yoonsikga@gmail.com>
2024-11-20 17:52:52 +0100

    random

---
+++ python-course/introduction/random.py
@@ -0,0 +1,10 @@
+import random as r
+
+# r.randint(1,6) # random number between 1 and 6
+# r.random() # random float between 0 and 1
+
+# options = ("rock", "paper", "scissors")
+# option = r.choice(options) # chooses random element
+
+# cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+# r.shuffle(cards) # returns the shuffled list
