c7a1a9bd7f11d4fe321c1e7394f87e73dd335716
yoonsik <yoonsikga@gmail.com>
2024-11-21 14:01:10 +0100

    match-case

---
+++ python-course/introduction/25-match-case.py
@@ -0,0 +1,6 @@
+# def day_of_week(day):
+#     match day: # need python 3.10 or higher
+#         case 1:
+#             return "It is monday"
+#         case 2:
+#             return "It is tuesday"
