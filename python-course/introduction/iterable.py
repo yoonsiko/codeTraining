9e406d445ee16a6b6772c5f7b2ce16d6406b692d
yoonsik <yoonsikga@gmail.com>
2024-11-21 13:18:51 +0100

    iterable

---
+++ python-course/introduction/iterable.py
@@ -0,0 +1,19 @@
+# Iterables = An object/collection that can return its elements one at a time,
+# allowing it to be iterated over in a loop
+
+# numbers = [1,2,3,4,5]
+
+# for number in numbers:
+#     print(number)
+
+# name = "Bro Code"
+# for char in name:
+#     print(char, end="")
+
+my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+for key in my_dictionary:
+    print(key)
+
+for value in my_dictionary.values():
+    print(value)
