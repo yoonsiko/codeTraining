981c5a807e4caafd5c2b96fa11c1e9f50dc26e7d
yoonsikga <yoonsikga@gmail.com>
2024-11-25 12:22:24 +0100

    Moved the loader function from app to more local where the data is loaded

--- react-course/20-react-router-exercise/frontend/src/pages/Events.js
+++ react-course/20-react-router-exercise/frontend/src/pages/Events.js
@@ -4,7 +4,16 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const fetchedEvents = useLoaderData();
-  return <EventsList events={fetchedEvents} />
+  return <EventsList events={fetchedEvents} />;
 }
 
 export default EventsPage;
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
