87bf835fb4a7f4e1c671bfd84dfc103ce6d1f211
yoonsikga <yoonsikga@gmail.com>
2024-11-25 15:19:05 +0100

    Implemented useRouteLoaderData Hook

--- react-course/20-react-router-exercise/frontend/src/pages/EditEvent.js
+++ react-course/20-react-router-exercise/frontend/src/pages/EditEvent.js
@@ -1,7 +1,10 @@
 import EventForm from '../components/EventForm'
+import { useRouteLoaderData } from 'react-router-dom';
 
 function EditEventPage() {
-  return <EventForm />;
+  const data = useRouteLoaderData('event-detail');
+  const event = data.event;
+  return <EventForm event={event} />;
 }
 
 export default EditEventPage;
--- react-course/20-react-router-exercise/frontend/src/pages/EventDetail.js
+++ react-course/20-react-router-exercise/frontend/src/pages/EventDetail.js
@@ -1,9 +1,9 @@
-import { useLoaderData } from "react-router-dom";
+import { useRouteLoaderData } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const data = useLoaderData();
+  const data = useRouteLoaderData('event-detail');
   return (
     <>
       <EventItem event={data.event} />
