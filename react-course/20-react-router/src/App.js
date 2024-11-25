9a02e42a797ecce087f933ff7db7c86385f176bd
yoonsikga <yoonsikga@gmail.com>
2024-11-25 09:46:04 +0100

    Changed the empty relative path to index route for homepage

--- react-course/20-react-router/src/App.js
+++ react-course/20-react-router/src/App.js
@@ -11,11 +11,11 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    errorElement: <ErrorPage/>,
+    errorElement: <ErrorPage />,
     children: [
-      { path: "", element: <HomePage /> },
+      { index: true, element: <HomePage /> },
       { path: "products", element: <ProductsPage /> },
-      { path: 'products/:productId', element: <ProductDetailPage/>}
+      { path: "products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
