304affad1d0049c09167378cc15ad50b0eb831c4
yoonsikga <yoonsikga@gmail.com>
2024-11-25 11:02:12 +0100

    Added link to every event to the respective event detail page

--- react-course/20-react-router-exercise/frontend/src/components/MainNavigation.js
+++ react-course/20-react-router-exercise/frontend/src/components/MainNavigation.js
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import classes from './MainNavigation.module.css';
+import { NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
@@ -7,10 +7,25 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/events">Events</Link>
+            <NavLink
+              to="/events"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Events
+            </NavLink>
           </li>
         </ul>
       </nav>
