8807b0fbb3377491d1fa404715d47e65a2322e40
yoonsikga <yoonsikga@gmail.com>
2024-11-25 13:17:00 +0100

    Added errorElement to router and throwing error if the fetch has failed

---
+++ react-course/20-react-router-exercise/frontend/src/pages/Error.js
@@ -0,0 +1,5 @@
+function ErrorPage() {
+  return <h1>An error has occured</h1>;
+}
+
+export default ErrorPage;
--- react-course/20-react-router-exercise/frontend/src/pages/Events.js
+++ react-course/20-react-router-exercise/frontend/src/pages/Events.js
@@ -4,7 +4,11 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
-  const fetchedEvents = data.events
+  const fetchedEvents = data.events;
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
   return <EventsList events={fetchedEvents} />;
 }
 
@@ -14,6 +18,8 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events" };
+    throw {message: "Could not fetch events."};
   } else {
     // const resData = await response.json();
     // const res = new Response(resData.events, {status: })
