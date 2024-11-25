18e0e47791f9eba6b6684c3cd50c9941482101d0
yoonsikga <yoonsikga@gmail.com>
2024-11-25 12:55:34 +0100

    Implemented useNavigation and state and directly returning the response object in the loader function

--- react-course/20-react-router-exercise/frontend/src/pages/Events.js
+++ react-course/20-react-router-exercise/frontend/src/pages/Events.js
@@ -3,17 +3,20 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const fetchedEvents = useLoaderData();
+  const data = useLoaderData();
+  const fetchedEvents = data.events
   return <EventsList events={fetchedEvents} />;
 }
 
 export default EventsPage;
+
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
   } else {
-    const resData = await response.json();
-    return resData.events;
+    // const resData = await response.json();
+    // const res = new Response(resData.events, {status: })
+    return response; // useLoaderData will automatically extract the data part of response object
   }
 }
--- react-course/20-react-router-exercise/frontend/src/pages/Root.js
+++ react-course/20-react-router-exercise/frontend/src/pages/Root.js
@@ -1,10 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 function RootLayout(){
+  const navigation = useNavigation(); // Is an object
+
+  // navigation.state === idle, loading or submitting
+
   return <>
     <MainNavigation/>
     <main>
+      {navigation.state === 'loading' && <p>Loading ...</p>}
       <Outlet/>
     </main>
   </>
