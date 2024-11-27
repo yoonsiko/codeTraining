2ee36da7c1a6e3297ed837a595fab783af5f9ac1
yoonsikga <yoonsikga@gmail.com>
2024-11-27 13:00:55 +0100

    Programmatically submitting data with delete

--- react-course/20-react-router-exercise/frontend/src/App.js
+++ react-course/20-react-router-exercise/frontend/src/App.js
@@ -25,8 +25,9 @@ import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from "./pages/EventDetail";
-import NewEventPage, {action as newEventAction} from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -56,6 +57,7 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
               { path: "edit", element: <EditEventPage /> },
             ],
--- react-course/20-react-router-exercise/frontend/src/pages/EventDetail.js
+++ react-course/20-react-router-exercise/frontend/src/pages/EventDetail.js
@@ -1,9 +1,9 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { useRouteLoaderData, redirect } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const data = useRouteLoaderData('event-detail');
+  const data = useRouteLoaderData("event-detail");
   return (
     <>
       <EventItem event={data.event} />
@@ -24,4 +24,18 @@ export async function loader({ request, params }) {
   } else {
     return response;
   }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method
+  });
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: "Could not fetch delete event" }),
+      { status: 500 }
+    );
+  }
+  return redirect("/events");
 }
