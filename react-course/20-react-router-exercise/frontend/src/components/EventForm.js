ba5d8d5253b96ed92f82e11f0c95f4cd49d48031
yoonsikga <yoonsikga@gmail.com>
2024-11-27 09:51:20 +0100

    Working with action(), post request and redirect

--- react-course/20-react-router-exercise/frontend/src/components/EventForm.js
+++ react-course/20-react-router-exercise/frontend/src/components/EventForm.js
@@ -1,30 +1,53 @@
-import { useNavigate } from 'react-router-dom';
+import { Form } from "react-router-dom";
 
-import classes from './EventForm.module.css';
+import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
-  const navigate = useNavigate();
   function cancelHandler() {
-    navigate('..');
+    // ...
   }
 
   return (
-    <form className={classes.form}>
+    <Form method='post' className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required defaultValue={event ? event.title: ''} />
+        <input
+          id="title"
+          type="text"
+          name="title"
+          required
+          defaultValue={event ? event.title : ""}
+        />
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" required defaultValue={event ? event.image: ''}/>
+        <input
+          id="image"
+          type="url"
+          name="image"
+          required
+          defaultValue={event ? event.image : ""}
+        />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required defaultValue={event ? event.date: ''}/>
+        <input
+          id="date"
+          type="date"
+          name="date"
+          required
+          defaultValue={event ? event.date : ""}
+        />
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description: ''}/>
+        <textarea
+          id="description"
+          name="description"
+          rows="5"
+          required
+          defaultValue={event ? event.description : ""}
+        />
       </p>
       <div className={classes.actions}>
         <button type="button" onClick={cancelHandler}>
@@ -32,7 +55,7 @@ function EventForm({ method, event }) {
         </button>
         <button>Save</button>
       </div>
-    </form>
+    </Form>
   );
 }
 
--- react-course/20-react-router-exercise/frontend/src/pages/NewEvent.js
+++ react-course/20-react-router-exercise/frontend/src/pages/NewEvent.js
@@ -1,11 +1,34 @@
+import { redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
-  function submitHandler(event){
-    event.preventDefault();
-    
-  }
-  return <EventForm/>;
+  return <EventForm />;
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+  // const enteredTitle = data.get('title');
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+  console.log(response);
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+      status: 500,
+    });
+  }
+  return redirect('/events');
+}
