56c65186a7f25e5a7ce628ef50ef0fd45b2a722f
yoonsikga <yoonsikga@gmail.com>
2024-11-27 13:53:04 +0100

    Implementation of useFetcher() to use the action of the route without rendering the page of the route

--- react-course/20-react-router-exercise/frontend/src/components/NewsletterSignup.js
+++ react-course/20-react-router-exercise/frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,34 @@
+import { useEffect } from "react";
 import classes from "./NewsletterSignup.module.css";
+import { useFetcher } from "react-router-dom";
 
 function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const { data, state } = fetcher;
+
+  // state === 'idle'
+  // state === 'loading'
+  // state === 'submitting'
+
+  useEffect(() => {
+    if (state === "idle" && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
