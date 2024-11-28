c51ca1311a3d17ca69ce3e4f2fc16a56b883f761
yoonsikga <yoonsikga@gmail.com>
2024-11-28 10:44:29 +0100

    Attaching Auth tokens to outgoing requests

--- react-course/21-authentication/frontend/src/pages/Authentication.js
+++ react-course/21-authentication/frontend/src/pages/Authentication.js
@@ -9,10 +9,10 @@ export default AuthenticationPage;
 
 export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
-  const mode = searchParams.get("mode") || 'login';
+  const mode = searchParams.get("mode") || "login";
 
-  if (mode !== 'login' && mode !== 'signup') {
-    throw new Response({message: 'Unsupported mode'}, {status: 422});
+  if (mode !== "login" && mode !== "signup") {
+    throw new Response({ message: "Unsupported mode" }, { status: 422 });
   }
 
   const data = await request.formData();
@@ -21,23 +21,32 @@ export async function action({ request }) {
     password: data.get("password"),
   };
 
-  const response = await fetch('http://localhost:8080/' + mode, {
-    method: 'POST',
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(authData)
+    body: JSON.stringify(authData),
   });
 
   if (response.status === 422 || response.status === 401) {
     return response;
   }
 
-  if (!response.ok){
-    throw new Response({message: 'Could not authenticate user'}, {status: 500});
+  if (!response.ok) {
+    throw new Response(
+      { message: "Could not authenticate user" },
+      { status: 500 }
+    );
   }
 
-  // soon: manage that token
-  return redirect("/")
+  const resData = await response.json();
+  const token = resData.token;
 
+  localStorage.setItem("token", token);
+
+  
+
+  // soon: manage that token
+  return redirect("/");
 }
