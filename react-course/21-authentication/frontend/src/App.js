badc146cab29891e77b11f0bce499b0a627e9667
yoonsikga <yoonsikga@gmail.com>
2024-11-28 12:46:15 +0100

    Adding user logout

--- react-course/21-authentication/frontend/src/App.js
+++ react-course/21-authentication/frontend/src/App.js
@@ -14,6 +14,7 @@ import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, {action as authAction} from "./pages/Authentication"
+import {action as logoutAction} from './pages/Logout'
 
 const router = createBrowserRouter([
   {
@@ -59,6 +60,10 @@ const router = createBrowserRouter([
         path: 'login',
         element: <AuthenticationPage />,
         action: authAction,
+      },
+      {
+        path: 'logout',
+        action: logoutAction,
       },
       {
         path: 'newsletter',
