0c7710189da06e699880abad36b42ecaaccff39f
yoonsikga <yoonsikga@gmail.com>
2024-12-16 09:27:42 +0100

    Added Link to navigate across pages singe page

--- react-course/24-nextjs/app/about/page.js
+++ react-course/24-nextjs/app/about/page.js
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function AboutPage() {
   return (
     <main>
       <p>Yeeeeet</p>
+      <p><Link href="../">Go back</Link></p>
     </main>
   );
 }
