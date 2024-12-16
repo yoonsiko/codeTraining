ef14d76262b959b4b1355151c6949c90ec14c061
yoonsikga <yoonsikga@gmail.com>
2024-12-16 11:21:19 +0100

    Outsourced the main header background from root layout to another component which is imported in the main header component including css from globals.css

--- react-course/24-nextjs/app/components/mainheader.js
+++ react-course/24-nextjs/app/components/mainheader.js
@@ -1,24 +1,30 @@
 import Link from "next/link";
-import logoImg from '@/assets/logo.png'
-import classes from './mainheader.module.css'
+import logoImg from "@/assets/logo.png";
+import classes from "./mainheader.module.css";
 import Image from "next/image";
+import MainHeaderBackground from "./mainheader-background";
 
 export default function MainHeader() {
-  return <header className={classes.header}>
-    <Link className={classes.logo} href="/">
-      <Image src={logoImg} alt="A plate with food" priority/>
-      NextLevel Food
-    </Link>
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food" priority />
+          NextLevel Food
+        </Link>
 
-    <nav className={classes.nav}>
-      <ul>
-        <li>
-          <Link href="/meals">Browse Meals</Link>
-        </li>
-        <li>
-          <Link href="/community">Foodies Community</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
