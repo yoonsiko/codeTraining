80a9eb0a085897df155fa65f58cada618bc0f3fc
yoonsikga <yoonsikga@gmail.com>
2025-01-02 10:48:02 +0100

    Outsourced the NavLink to own client component

--- react-course/24-nextjs/app/components/mainheader.js
+++ react-course/24-nextjs/app/components/mainheader.js
@@ -3,8 +3,10 @@ import logoImg from "@/assets/logo.png";
 import classes from "./mainheader.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./mainheader-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
+
   return (
     <>
       <MainHeaderBackground />
@@ -17,10 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
