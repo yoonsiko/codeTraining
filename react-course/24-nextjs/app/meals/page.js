ab569c4a1a548d14ccafdb5c9925b25fb0fe5d09
yoonsikga <yoonsikga@gmail.com>
2025-01-02 11:00:57 +0100

    Outputting meals data and images with unknown dimension

--- react-course/24-nextjs/app/meals/page.js
+++ react-course/24-nextjs/app/meals/page.js
@@ -1,13 +1,24 @@
 import Link from "next/link";
+import classes from "./page.module.css";
 
 export default function MealsPage() {
-  return <main>
-    <h1>Meals!</h1>
-    <p><Link href="/meals/meal-1">Meal 1</Link></p>
-    <p><Link href="/meals/meal-2">Meal 2</Link></p>
-    <p><Link href="/meals/share">Share</Link></p>
-    
-    
-    
-  </main>
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        
+      </main>
+    </>
+  );
 }
